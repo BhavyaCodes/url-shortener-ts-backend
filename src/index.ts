edce4d97da59930a467a8fa41591667eb24285f5
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-09 23:44:01 +0530

    add cors, error handler

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 require("dotenv").config();
 import mongoose from "mongoose";
 import routes from "./routes";
-import express, {
-  RequestHandler,
-  Request,
-  Response,
-  NextFunction,
-  json,
-} from "express";
+import cors from "cors";
+
+import express, { Request, Response, NextFunction, json } from "express";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(cors());
 app.use(json());
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({ hi: "There" });
 });
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: err.message });
+});
 
 mongoose
   .connect(process.env.DB_URL as string, {
