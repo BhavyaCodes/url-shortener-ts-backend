b411bd169d61a34776afafe87583bf9805996a5e
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-09 22:40:51 +0530

    add mongoose model

---
+++ src/model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document } from "mongoose";
+import shortid from "shortid";
+
+export interface IUrl extends Document {
+  full: string;
+  short: string;
+}
+
+export const UrlSchema = new Schema({
+  full: {
+    required: true,
+    type: String,
+  },
+  short: {
+    required: true,
+    type: String,
+    default: shortid.generate(),
+  },
+});
+
+const Url = mongoose.model<IUrl>("Url", UrlSchema);
+export default Url;
