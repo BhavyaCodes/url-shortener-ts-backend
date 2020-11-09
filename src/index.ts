5819d18f279e237c4b26bb926b551c4bcd6d8580
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-09 22:22:49 +0530

    boiler plate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("ok");
+import express, {
+  RequestHandler,
+  Request,
+  Response,
+  NextFunction,
+  json,
+} from "express";
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(json());
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ hi: "There" });
+});
+
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
