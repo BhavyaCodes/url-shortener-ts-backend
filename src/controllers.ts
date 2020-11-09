992bdd2d9dd52e9b5fb1c8244c915729f781454a
Juggernau9 <xbhavyatomar@gmail.com>
2020-11-09 23:14:54 +0530

    finish endpoint for creating url

--- src/controllers.ts
+++ src/controllers.ts
@@ -3,10 +3,15 @@ import { RequestHandler } from "express";
 import { IUrl } from "./model";
 
 export const create: RequestHandler = async (req, res, next) => {
+  const existing = await Url.findOne({ full: req.body.url }).lean();
+  if (existing) {
+    res.json(existing);
+    return;
+  }
+
   const url = new Url({
     full: req.body.url,
   });
   const savedUrl: IUrl = await url.save();
-  res.json({ savedUrl });
-  // console.log(req.body);
+  res.json({ url: savedUrl });
 };
